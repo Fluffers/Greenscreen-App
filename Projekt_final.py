23532073b4a4f4bb8eb8ae7b0fe2be24dd4fe7c4
Kuba Juszczak <kuborok123434@gmail.com>
2021-02-20 21:24:16 +0100

    Visual edits
    color functions merged, visual fixes, few bug preventions

--- Projekt_final.py
+++ Projekt_final.py
@@ -7,28 +7,29 @@ root = Tk()
 
 # rozmiar okna
 root.title("Greenscreen App")
-root.geometry("310x620")
+root.geometry("310x420")
+root.resizable(False, False)
 
 # polecenie - film
 header = Label(root, text="Greenscreen App", font=('Calibri', 15, 'bold'))
 text1 = Label(root, text=" Podaj ścieżkę do filmu: ", font=('Calibri', 12))
-subtext1 = Label(root, text=" razem z rozszerzeniem ", font=('Calibri', 8, 'italic'))
+subtext1 = Label(root, text=" brak ścieżki będzie oznaczać kamerę urządzenia ", font=('Calibri', 8, 'italic'))
 header.pack(padx=20, pady=10)
 text1.pack(padx=20, pady=4)
 subtext1.pack(padx=20, pady=4)
 
 # input filmu
 video_name = Entry(root, width=100)
+video_name.insert(END, 'example-video.mp4')
 video_name.pack(anchor=W, padx=20)
 
 # polecenie - tło
 text2 = Label(root, text=" Podaj ścieżkę do tła: ", font=('Calibri', 12))
-subtext2 = Label(root, text=" razem z rozszerzeniem  ", font=('Calibri', 8, 'italic'))
 text2.pack(padx=20, pady=4)
-subtext2.pack(padx=20, pady=4)
 
 # input tła
 img = Entry(root, width=100)
+img.insert(END, 'example-image.jpg')
 img.pack(padx=20, pady=10)
 
 # polecenie - kolorek
@@ -46,140 +47,70 @@ c3.pack()
 
 # uruchomienie maleństwa
 butt = Button(root, text="  Zaczynamy!  ", command=lambda: green_screen(video_name.get(), img.get(), c.get()))
-butt.pack(side=BOTTOM, pady=100)
+butt.pack(padx=20, pady=30)
 
-def zielona(count, background, video):
-    try:
-        while (video.isOpened()):
-            ret, movie = video.read()
+def show_video(video, background, color, fps):
+
+    while (video.isOpened()):
+        ret, movie = video.read()
+
+        if ret:
             movie = cv2.resize(movie, (640, 480))
             background = cv2.resize(background, (640, 480))
-
-            if not ret:
-                break
-            count += 1
-
             # Zamiana rgb na hsv
             hsv = cv2.cvtColor(movie, cv2.COLOR_BGR2HSV)
 
-            # Zielona maska
-            lower_green = np.array([35, 40, 40])
-            upper_green = np.array([86, 255, 255])
-            gmask1 = cv2.inRange(hsv, lower_green, upper_green)
+            if color == 1:
+                lower_green = np.array([35, 40, 40])
+                upper_green = np.array([86, 255, 255])
+                mask1 = cv2.inRange(hsv, lower_green, upper_green)
 
-            # Fuzja i przetwarzanie maski
-            gmask1 = cv2.morphologyEx(gmask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
-            gmask1 = cv2.dilate(gmask1, np.ones((3, 3), np.uint8), iterations=1)
-            gmask2 = cv2.bitwise_not(gmask1)
+            elif color == 2:
+
+                lower_red = np.array([170, 40, 40])
+                upper_red = np.array([179, 255, 255])
+                mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+                lower_red2 = np.array([0, 40, 40])
+                upper_red2 = np.array([13, 255, 255])
+                mask_2 = cv2.inRange(hsv, lower_red2, upper_red2)
+
+                mask1 = mask1 + mask_2
+            else:
+                lower_blue = np.array([95, 40, 40])
+                upper_blue = np.array([135, 255, 255])
+                mask1 = cv2.inRange(hsv, lower_blue, upper_blue)
+
+            mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
+            mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations=1)
+            mask2 = cv2.bitwise_not(mask1)
 
             # Widok wyjściowy
-            res1 = cv2.bitwise_and(background, background, mask=gmask1)
-            res2 = cv2.bitwise_and(movie, movie, mask=gmask2)
+            res1 = cv2.bitwise_and(background, background, mask=mask1)
+            res2 = cv2.bitwise_and(movie, movie, mask=mask2)
             final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
 
             cv2.imshow('Aby zakonczyc, nacisnij ESC', final_output)
 
-            # kończymy z Esc
-            k = cv2.waitKey(25)
-            if k == 27:
-                exit()
-    except():
-        print("Exit")
-        exit()
-
-def czerwona(count, background, video):
-    try:
-        while (video.isOpened()):
-            ret, movie2 = video.read()
-            movie2 = cv2.resize(movie2, (640, 480))
-            background = cv2.resize(background, (640, 480))
-
-            if not ret:
+            # Wychodzimy wciskając 'q' // jedna klatka trwa tyle, na ile pozwala wideo na wejściu
+            if cv2.waitKey(int((1 / int(fps)) * 1000)) & 0xFF == ord('q'):
                 break
-            count += 1
+        else:
+            break
 
-            hsv = cv2.cvtColor(movie2, cv2.COLOR_BGR2HSV)
-
-            # Czerwona maska
-            lower_red = np.array([170, 40, 40])
-            upper_red = np.array([179, 255, 255])
-            rmask1 = cv2.inRange(hsv, lower_red, upper_red)
-
-            lower_red2 = np.array([0, 40, 40])
-            upper_red2 = np.array([13, 255, 255])
-            rmask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-
-            rmask = rmask1 + rmask2
-
-            rmask = cv2.morphologyEx(rmask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
-            rmask = cv2.dilate(rmask, np.ones((3, 3), np.uint8), iterations=1)
-            rmask3 = cv2.bitwise_not(rmask)
-
-            res1 = cv2.bitwise_and(background, background, mask=rmask)
-            res2 = cv2.bitwise_and(movie2, movie2, mask=rmask3)
-            final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
-
-            cv2.imshow('Aby zakonczyc, nacisnij ESC', final_output)
-
-            k = cv2.waitKey(25)
-            if k == 27:
-                exit()
-            else:
-                continue
-    except:
-        print("Exit")
-        exit()
-
-def niebieska(count, background, video):
-    try:
-        while (video.isOpened()):
-            ret, movie3 = video.read()
-            movie3 = cv2.resize(movie3, (640, 480))
-            background = cv2.resize(background, (640, 480))
-
-            if not ret:
-                break
-            count += 1
-
-            hsv = cv2.cvtColor(movie3, cv2.COLOR_BGR2HSV)
-
-            # Niebieska maska
-            lower_blue = np.array([95, 40, 40])
-            upper_blue = np.array([135, 255, 255])
-            bmask1 = cv2.inRange(hsv, lower_blue, upper_blue)
-
-            bmask1 = cv2.morphologyEx(bmask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
-            bmask1 = cv2.dilate(bmask1, np.ones((3, 3), np.uint8), iterations=1)
-            bmask2 = cv2.bitwise_not(bmask1)
-
-            res1 = cv2.bitwise_and(background, background, mask=bmask1)
-            res2 = cv2.bitwise_and(movie3, movie3, mask=bmask2)
-            final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
-
-            cv2.imshow('Aby zakonczyc, nacisnij ESC', final_output)
-
-            k = cv2.waitKey(25)
-            if k == 27:
-                exit()
-            else:
-                continue
-    except:
-        print("Exit")
-        exit()
-
-def green_screen(video_name, img, c):
+def green_screen(video_name, img, color):
+    if video_name == '':
+        video_name = 0
     video = cv2.VideoCapture(video_name)
+    fps = video.get(cv2.CAP_PROP_FPS)
     background = cv2.imread(img)
+
     # Czas dla kamery
     time.sleep(2)
     count = 0
 
-    if c == 1:
-        print(zielona(count, background, video))
-    if c == 2:
-        print(czerwona(count, background, video))
-    if c == 3:
-        print(niebieska(count, background, video))
+    show_video(video, background, color, fps)
+
 
     video.release()
     cv2.destroyAllWindows()
